2375c67b6d2787a3c71d2a9c6d28b3f78c02bf89
sem7655 <127785162+sem7655@users.noreply.github.com>
2023-08-20 02:52:51 +0600

    Create tg.py

---
+++ tg.py
@@ -0,0 +1,40 @@
+hi = 'привет, это курс по python в telegram, сегодня мы научимся работать с командой print() - кстати то, что ты читаешь, было напечатано через print'
+print(hi)
+
+task = 'я дам тебе задание, напиши print("Hello_word") '
+print(task)
+
+while True:
+    task1 = input()
+    if task1 == 'print("Hello_word")' or task1 =='print("hello_word")' or task1 == "print('Hello_word')":
+        print('да супер')
+        print('ты хочешь продолжить учится да или нет')
+        break
+    else:
+        print('попробуй еще обрати внимание на буквы, если ты написал не так, то код не сработает. Пиши все так, как было сказано выше!')
+        print(task)
+
+
+def check_answer(answer):
+    # Проверяем, правильный ли ответ
+    # Здесь можно добавить свои условия проверки
+    if answer == "правильный ответ":
+        return True
+    else:
+        return False
+
+
+def continue_learning():
+    # Запросить у пользователя, хочет ли он продолжить изучение
+    response = input("Хотите продолжить изучение? (Да/Нет): ")
+    if response.lower() == "да":
+        return True
+    else:
+        return False
+
+
+def prompt_ide():
+    # Вывести сообщение о непонимании кода IDE в Telegram
+    print("К сожалению, IDE в Telegram не может полноценно понимать ваш код.")
+    print("Рекомендуем писать код в своем редакторе или использовать онлайн-компилятор.")
+    print("Вот ссылка, где можно удобно писать код онлайн: https://www.programiz.com/python-programming/online-compiler/")
