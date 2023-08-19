d5c8d27f5215f8334c2071f728f9e4f8d3f0be2d
sem7655 <127785162+sem7655@users.noreply.github.com>
2023-08-20 03:17:17 +0600

    Create error.py

---
+++ error.py
@@ -0,0 +1,20 @@
+hi = 'привет, это курс по python в telegram, сегодня мы научимся работать с командой print() - кстати то, что ты читаешь, было напечатано через print'
+print(hi)
+
+task = 'я дам тебе задание, напиши print("Hello_word") '
+print(task)
+
+while True:
+    task1 = input()
+    if task1 == 'print("Hello_word")' or task1 =='print("hello_word")' or task1 == "print('Hello_word')":
+        print('да супер')
+        response = input("Хотите продолжить изучение? (Да/Нет): ")
+        if response.lower() == "да":
+    # Вывести сообщение о непонимании кода IDE в Telegram
+    print("К сожалению, IDE в Telegram не может полноценно понимать ваш код.")
+    print("Рекомендуем писать код в своем редакторе или использовать онлайн-компилятор.")
+    print(
+        "Вот ссылка, где можно удобно писать код онлайн: https://www.programiz.com/python-programming/online-compiler/")
+else:
+    print('попробуй еще обрати внимание на буквы, если ты написал не так, то код не сработает. Пиши все так, как было сказано выше!')
+    print(task)
