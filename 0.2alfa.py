57320d12225c60e89410eaa715511663ae015205
sem7655 <127785162+sem7655@users.noreply.github.com>
2023-08-20 13:38:41 +0600

    Create 0.2alfa.py

---
+++ 0.2alfa.py
@@ -0,0 +1,26 @@
+hi = 'привет, это курс по python в telegram, сегодня мы научимся работать с командой print() - кстати то, что ты читаешь, было напечатано через print'
+print(hi)
+
+task = 'я дам тебе задание, напиши print("Hello_word") '
+print(task)
+
+while True:
+    task1 = input()
+    if task1 == 'print("Hello_word")' or task1 == 'print("hello_word")' or task1 == "print('Hello_word')":
+        print('да супер')
+        print('ты хочешь продолжить учится да или нет')
+        response = input("Хотите продолжить изучение? (Да/Нет): ")
+        if response.lower() == "да":
+            break
+    else:
+        print('попробуй еще, обрати внимание на буквы. Если ты написал не так, то код не сработает. Пиши все так, как было сказано выше!')
+        print(task)
+
+print("К сожалению, IDE в Telegram не может полноценно понимать ваш код.")
+print("Рекомендуем писать код в своем редакторе или использовать онлайн-компилятор.")
+import webbrowser
+import urllib.parse
+query = 'https://www.programiz.com/python-programming/online-compiler/'
+query = urllib.parse.quote(query)
+url = "https://www.google.com/search?q=" + query
+webbrowser.open(url)
